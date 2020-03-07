a195461854800c155aaaf791058fc9848e9aa08e
hakan saraç <hakansrc@msn.com>
2020-03-07 19:36:35 +0300

    raporda oldukça ilerledim. 1. ve 2. soruyu yazdım, 3 zaten 2'ye oldukça benzer, 4 için teoriyi yazdıktan sonra 5'için gif'i koymak kalıcak.

--- Project1/Project1_analyticalmodel.m
+++ Project1/Project1_analyticalmodel.m
@@ -1,6 +1,6 @@
 clear
 clc
-
+close all
 
 u0 = 4*pi*10^-7;
 deltatheta = 0.01;
@@ -39,7 +39,7 @@ for i=(find(theta==102.64):1:(find(theta==180)))
     R2_theta_part3(i) = R2coeff_part3*(360/(180-theta(i)));
     Req(i) = R1_theta_part3(i)*R2_theta_part3(i)/(R1_theta_part3(i)+R2_theta_part3(i));
 end
-%% reluctance over 360 degrees
+%% copy the 0-180 degree reluctance to 180-360 degree
 Req(1) = Req(2);
 Req(numel(theta)) = Req(numel(theta)-1);
 for(i=(numel(theta)+1):(2*numel(theta)-1))
@@ -48,13 +48,12 @@ for(i=(numel(theta)+1):(2*numel(theta)-1))
 end
 
 %% Resultant inductance
-
 Leq = (N*N)./Req;
 
 %% Torque generated 
 for i=1:((2*numel(theta)-1))
     if(i>1)
-        Teq(i) = 0.5*(Idc*Idc)*(Leq(i)-Leq(i-1))/deltatheta;
+        Teq(i) = 0.5*(Idc*Idc)*(Leq(i)-Leq(i-1))/(deltatheta*2*pi/360);
     else
         Teq(i) = 0;%0.5*(Idc*Idc)*(Leq(i))/deltatheta;
     end
@@ -63,12 +62,71 @@ end
 Teq(10265) = 0; % there was an undesired spike, just cover that up;
 Teq(18002) = 0; % there was an undesired spike, just cover that up;
 Teq(28266) = 0; % there was an undesired spike, just cover that up;
-
+%% Sample the results for plotting with FEA Results
 plot(Teq)
+for(i=1:360)
+    
+    TeqSample(i) = Teq(i*100);
+    LeqSample(i) = Leq(i*100);
+    ReqSample(i) = Req(i*100);
+end
+%% load the fea results
+load('Report/Q3_figures/Q3TorqueInductanceDataMaxwell.mat');
+subplot(2,1,1);
+plot(LeqSample,'LineWidth',2);
+hold on
+plot(InductancePlot1.VarName2/1000,'LineWidth',2);
+hold off
+xlabel('Rotor angle (degrees)','FontSize',12,'FontWeight','bold');
+ylabel('Inductance (Henry)','FontSize',12,'FontWeight','bold');
+set(gca,'FontSize',12,'FontWeight','bold')
+grid on
+xlim([0 360])
+xticks([0 30 60 90 120 150 180 210 240 270 300 330 360])
+legend('Analytical Result','FEA Result')
+% figure
+subplot(2,1,2);
+plot(TeqSample,'LineWidth',2);
+hold on
+plot(TorquePlot2Copy.Plot/1000,'LineWidth',2);
+hold off
+xlabel('Rotor angle (degrees)','FontSize',12,'FontWeight','bold');
+ylabel('Torque (Nm)','FontSize',12,'FontWeight','bold');
+set(gca,'FontSize',12,'FontWeight','bold')
+grid on
+xlim([0 360])
+xticks([0 30 60 90 120 150 180 210 240 270 300 330 360])
+legend('Analytical Result','FEA Result')
+%% Q1
+%a
+figure
+subplot(3,1,1)
+plot(ReqSample,'LineWidth',2);
+xlabel('Rotor angle (degrees)','FontSize',12,'FontWeight','bold');
+ylabel('Reluctance (Amperes/Weber)','FontSize',12,'FontWeight','bold');
+set(gca,'FontSize',12,'FontWeight','bold')
+grid on
+xlim([0 360])
+xticks([0 30 60 90 120 150 180 210 240 270 300 330 360])
 
+% figure
+subplot(3,1,2)
+plot(LeqSample,'LineWidth',2);
+xlabel('Rotor angle (degrees)','FontSize',12,'FontWeight','bold');
+ylabel('Inductance (Henry)','FontSize',12,'FontWeight','bold');
+set(gca,'FontSize',12,'FontWeight','bold')
+grid on
+xlim([0 360])
+xticks([0 30 60 90 120 150 180 210 240 270 300 330 360])
 
-
-
-
+% figure
+subplot(3,1,3)
+plot(TeqSample,'LineWidth',2);
+xlabel('Rotor angle (degrees)','FontSize',12,'FontWeight','bold');
+ylabel('Torque (Nm)','FontSize',12,'FontWeight','bold');
+set(gca,'FontSize',12,'FontWeight','bold')
+grid on
+xlim([0 360])
+xticks([0 30 60 90 120 150 180 210 240 270 300 330 360])
 
 
