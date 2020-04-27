6f1a4bb06903a13c0381cb40132db31a7ec5fcf7
hakan saraç <hakansrc@msn.com>
2020-04-28 01:11:31 +0300

    1. soruda ilerlendi. rapor başlandı.

--- Project3/Q1/Q1_routine.m
+++ Project3/Q1/Q1_routine.m
@@ -12,10 +12,27 @@ MagnetRelativePermeability = 1.05;
 MagnetResidualFluxDensity = 1.32; %Tesla, N42 material
 PoleArea = pi*RotorDiameter*MotorAxialLength/NumberOfPoles; %m2
 MagnetAreaPerPole = PoleArea*MagnetToPolePitchRatio;
+NonMagnetAreaPerPole = (1-MagnetToPolePitchRatio)*PoleArea;
+
+ 
 
 %total reluctance seen by the magnets are: 
 %assuming that the core is infinitely permeable
 %assuming that the magnets are on the surface of the rotor
-TotalMagnetReluctance = MagnetThickness/(MagnetAreaPerPole*MagnetRelativePermeability)+2*((AirGapClearance*2+MagnetThickness)/(u0*PoleArea*(1-MagnetToPolePitchRatio)));
-IdealFluxMagnets = MagnetAreaPerPole*MagnetResidualFluxDensity;
-MMFmagnet = IdealFluxMagnets*TotalMagnetReluctance;
+%Rm: reluctance of magnet's itself
+%Rag1: reluctance from magnet's top to stator
+%Rag2: reluctance of the return path
+Rm1 = MagnetThickness/(MagnetAreaPerPole*u0*MagnetRelativePermeability);
+Rm2 = Rm1;
+Rag1 = AirGapClearance/(MagnetAreaPerPole*u0);
+Rag2 = Rag1;
+
+MmfPerMagnet = MagnetAreaPerPole*MagnetResidualFluxDensity*Rm1;
+AirGapFlux = (MmfPerMagnet*2)/(Rm1+Rm2+Rag1+Rag2);
+AirGapFluxDensity = AirGapFlux/MagnetAreaPerPole;
+
+Bm = AirGapFluxDensity;
+Br = MagnetResidualFluxDensity;
+Hm = (Bm-Br)/(MagnetRelativePermeability*u0);
+
+
