bc5231f9f1a2486fb77759e892d9059c568fe169
hakan saraç <hakansrc@msn.com>
2020-04-29 00:24:15 +0300

    1. soru bitti, fea sonuçları alındı, yorumlama ve hikaye kısmı kaldı.

--- Project3/Q1/Q1_routine.m
+++ Project3/Q1/Q1_routine.m
@@ -1,3 +1,7 @@
+close all
+clear
+clc
+load("MagB")
 NumberOfPhases = 3;
 NumberOfPoles = 4;
 MotorAxialLength = 100e-3; % meters
@@ -27,6 +31,8 @@ Rm2 = Rm1;
 Rag1 = AirGapClearance/(MagnetAreaPerPole*u0);
 Rag2 = Rag1;
 
+
+
 MmfPerMagnet = MagnetAreaPerPole*MagnetResidualFluxDensity*Rm1;
 AirGapFlux = (MmfPerMagnet*2)/(Rm1+Rm2+Rag1+Rag2);
 AirGapFluxDensity = AirGapFlux/MagnetAreaPerPole;
@@ -35,4 +41,24 @@ Bm = AirGapFluxDensity;
 Br = MagnetResidualFluxDensity;
 Hm = (Bm-Br)/(MagnetRelativePermeability*u0);
 
+MagneticLoading = NumberOfPoles*AirGapFlux/(pi*RotorDiameter*MotorAxialLength);
 
+plot(360*MagB.Distance/(pi*104.5),MagB.MagB,'LineWidth',6);
+xlabel('Rotor angle (degrees)','FontSize',12,'FontWeight','bold');
+ylabel('Flux Density (T)','FontSize',12,'FontWeight','bold');
+set(gca,'FontSize',12,'FontWeight','bold')
+grid on
+xlim([0 360])
+xticks([0 30 60 90 120 150 180 210 240 270 300 330 360])
+res = 300;
+size = [12000 6000];
+set(gcf,'paperunits','inches','paperposition',[0 0 size/res]);
+print('resized.tiff','-dtiff',['-r' num2str(res)]);
+% hold on
+% plot(360*MagB.Distance(56)/(pi*104.5),MagB.MagB(56),'+','markersize',12,'linewidth',2)
+%legend('Analytical Result','FEA Result')
+MagB_mean = 0;
+for i =1:numel(MagB.MagB)
+    MagB_mean = MagB_mean+ MagB.MagB(i);
+end
+MagB_mean = MagB_mean/numel(MagB.MagB);
